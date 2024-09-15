b44e573725d64d1f4c282b8a3b99450b29823ef7
SomeshChevella <chevellavsomeshwar@gmail.com>
2024-09-14 23:30:55 -0400

    Initiate SpringBoot Project, Create Model, Service, Controller,ServiceTest for User

---
+++ src/test/java/com/somesh/user_service/services/UserServiceTest.java
@@ -0,0 +1,16 @@
+package com.somesh.user_service.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void save() {
+    }
+}
